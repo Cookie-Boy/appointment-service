f2c6d80a95bb6a317aafc6157265cbe8858beab5
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-04-28 20:21:16 +0700

    Edit RedisConfig.java

--- src/main/java/ru/sibsutis/appointment/configuration/RedisConfig.java
+++ src/main/java/ru/sibsutis/appointment/configuration/RedisConfig.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Slf4j
@@ -20,6 +22,15 @@ public class RedisConfig {
 
     @Value("${spring.redis.port}")
     private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        log.info("Trying to create bean. Redis host: {}, port: {}", redisHost, redisPort);
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        return new LettuceConnectionFactory(config);
+    }
 
     @PostConstruct
     public void checkRedisConnection() {
